682f3ccccd5f369ff45ef7eb9eeade55c682c116
Jorrit Salverda <jsalverda@travix.com>
2019-12-04 10:27:23 +0100

    remove comment

--- api/v1/cluster_types.go
+++ api/v1/cluster_types.go
@@ -17,44 +17,6 @@ type ClusterSpec struct {
 
 	// The internal URL to the estafette-ci-api service for build / release jobs to report to.
 	InternalURL string `json:"internalURL"`
-
-
-
-
-
-
-    // apiServer:
-    //   baseURL: https://estafette.travix.com/
-    //   serviceURL: https://estafette.tooling.internal.travix.io/
-
-    // auth:
-    //   iap:
-    //     enable: true
-    //     audience: /projects/1012027921753/global/backendServices/2110500691870907819
-    //   apiKey: estafette.secret(F-GHrep4UAYEVH18.fhWaqbptXBp6Zc0jBFaisNwSP2TTqjzwKK6uD8_EomjU6JY_3t21uaRf7lzcUA==)
-
-    // jobs:
-    //   namespace: estafette-ci-jobs
-    //   minCPUCores: 0.1
-    //   maxCPUCores: 7.0
-    //   cpuRequestRatio: 1.0
-    //   minMemoryBytes: 67108864
-    //   maxMemoryBytes: 21474836480
-    //   memoryRequestRatio: 1.25
-
-	// database:
-    //   databaseName: estafette_ci_api
-    //   host: cockroachdb-public.estafette.svc.cluster.local
-    //   insecure: true
-    //   certificateDir: /cockroach-certs
-    //   port: 26257
-    //   user: estafette_ci_api
-    //   password: estafette.secret(PopOsWdYqBKsgc1f.6KmE3-UWIujjGHGutu6hIbtUdnCOswO2xA79gEhN3emoNs6pyktFH6n4rB4msA==)
-
-
-	//   # registryMirror: {{.REGISTRY_MIRROR}}
-	//   dindMtu: {{.DIND_MTU}}
-
 }
 
 // ClusterStatus defines the observed state of Cluster
