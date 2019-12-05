f320873d5ff0e0c1cefc2ff50149abca7754cbd5
Vitor Arins <vitorarins@gmail.com>
2019-12-05 16:00:04 +0100

    Add fields to trusted image spec.

--- api/v1/trustedimage_types.go
+++ api/v1/trustedimage_types.go
@@ -8,9 +8,27 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // TrustedImageSpec defines the desired state of TrustedImage
+// +k8s:openapi-gen=true
 type TrustedImageSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// +kubebuilder:validation:Required
+	Type string `json:"type"`
+	// +kubebuilder:validation:Optional
+	WhitelistedPipelines string `json:"whitelistedPipelines,omitempty"`
+	// +kubebuilder:validation:Required
+	Path string `json:"path"`
+	// +kubebuilder:validation:Optional
+	RunPrivileged bool `json:"runPrivileged"`
+	// +kubebuilder:validation:Optional
+	RunDocker bool `json:"runDocker"`
+	// +kubebuilder:validation:Optional
+	AllowCommands bool `json:"allowCommands"`
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:UniqueItems
+	InjectedCredentialTypes []string `json:"injectedCredentialTypes,omitempty"`
 }
 
 // TrustedImageStatus defines the observed state of TrustedImage
