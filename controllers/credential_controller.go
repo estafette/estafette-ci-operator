701a084842d1265ce381be56116754c0045093a0
Vitor Arins <vitorarins@gmail.com>
2020-01-14 17:16:20 +0100

    Add some of the inital controller logic fro credentials.

--- controllers/credential_controller.go
+++ controllers/credential_controller.go
@@ -4,10 +4,16 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	civ1 "github.com/estafette/estafette-ci-operator/api/v1"
+)
+
+var (
+	configMapOwnerKey = ".metadata.controller"
+	apiGVStr          = civ1.GroupVersion.String()
 )
 
 // CredentialReconciler reconciles a Credential object
@@ -18,12 +24,28 @@ type CredentialReconciler struct {
 
 // +kubebuilder:rbac:groups=ci.estafette.io,resources=credentials,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=ci.estafette.io,resources=credentials/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=configmaps/status,verbs=get
 
 func (r *CredentialReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("credential", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("credential", req.NamespacedName)
 
-	// your logic here
+	var credential civ1.Credential
+	if err := r.Get(ctx, req.NamespacedName, &credential); err != nil {
+		log.Error(err, "unable to fetch Credential")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var childConfigMaps corev1.ConfigMapList
+	if err := r.List(ctx, &childConfigMaps, client.InNamespace(req.Namespace), client.MatchingFields{configMapOwnerKey: req.Name}); err != nil {
+		log.Error(err, "unable to list child ConfigMaps")
+		return ctrl.Result{}, err
+	}
+	if len(childConfigMaps.Items) <= 0 {
+		// TODO: create new config map here
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
 }
