3fb0a12585c3719c320c65711d52981f83088c28
Vitor Arins <vitorarins@gmail.com>
2020-01-23 16:33:55 +0100

    Ad initial implementation of controller logic.

--- controllers/credential_controller.go
+++ controllers/credential_controller.go
@@ -2,13 +2,16 @@ package controllers
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/go-logr/logr"
-	corev1 "k8s.io/api/core/v1"
+	yaml "gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	civ1 "github.com/estafette/estafette-ci-operator/api/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -19,7 +22,8 @@ var (
 // CredentialReconciler reconciles a Credential object
 type CredentialReconciler struct {
 	client.Client
-	Log logr.Logger
+	Log    logr.Logger
+	Scheme *runtime.Scheme
 }
 
 // +kubebuilder:rbac:groups=ci.estafette.io,resources=credentials,verbs=get;list;watch;create;update;patch;delete
@@ -43,15 +47,85 @@ func (r *CredentialReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 		return ctrl.Result{}, err
 	}
 	if len(childConfigMaps.Items) <= 0 {
-		// TODO: create new config map here
-		return ctrl.Result{}, nil
+
+		configmap, err := r.constructConfigMapForCredential(&credential)
+		if err != nil {
+			log.Error(err, "unable to construct configmap from credential")
+			// don't bother requeuing until we get a change to the spec
+			return ctrl.Result{}, nil
+		}
+		if err := r.Create(ctx, configmap); err != nil {
+			log.Error(err, "unable to create ConfigMap for Credential", "configmap", configmap)
+			return ctrl.Result{}, err
+		}
+
+		log.V(1).Info("created ConfigMap for Credential run", "configmap", configmap)
 	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *CredentialReconciler) constructConfigMapForCredential(credential *civ1.Credential) (*corev1.ConfigMap, error) {
+	name := fmt.Sprintf("%s-credential", credential.Name)
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      make(map[string]string),
+			Annotations: make(map[string]string),
+			Name:        name,
+			Namespace:   credential.Namespace,
+		},
+		Data: make(map[string]string),
+	}
+
+	credentialsData := make(map[string]interface{})
+	credentialConfig := make(map[string]interface{})
+	credentialConfig["name"] = credential.ObjectMeta.Name
+	credentialConfig["type"] = credential.Spec.Type
+	credentialConfig["whitelistedTrustedImages"] = credential.Spec.WhitelistedTrustedImages
+	credentialConfig["whitelistedPipelines"] = credential.Spec.WhitelistedPipelines
+	for k, v := range credential.Spec.AdditionalProperties {
+		credentialConfig[k] = v
+	}
+
+	credentialsData["credentials"] = []map[string]interface{}{credentialConfig}
+	credentialsYaml, err := yaml.Marshal(credentialsData)
+	if err != nil {
+		return nil, err
+	}
+	cm.Data["credentials-config.yaml"] = string(credentialsYaml)
+
+	for k, v := range credential.ObjectMeta.Labels {
+		cm.ObjectMeta.Labels[k] = v
+	}
+	if err := SetControllerReferences(credential, cm, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return cm, nil
+}
+
 func (r *CredentialReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	if err := mgr.GetFieldIndexer().IndexField(&corev1.ConfigMap{}, configMapOwnerKey, func(rawObj runtime.Object) []string {
+		// grab the configmap object, extract the owner...
+		configmap := rawObj.(*corev1.ConfigMap)
+		owner := metav1.GetControllerOf(configmap)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a Credential...
+		if owner.APIVersion != apiGVStr || owner.Kind != "Credential" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&civ1.Credential{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
---
+++ controllers/util.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
+
+func SetControllerReferences(owner, controlled metav1.Object, scheme *runtime.Scheme) error {
+	ro, ok := owner.(runtime.Object)
+	if !ok {
+		return fmt.Errorf("%T is not a runtime.Object, cannot call SetControllerReference", owner)
+	}
+
+	ownerNs := owner.GetNamespace()
+	if ownerNs != "" {
+		objNs := controlled.GetNamespace()
+		if objNs == "" {
+			return fmt.Errorf("cluster-scoped resource must not have a namespace-scoped owner, owner's namespace %s", ownerNs)
+		}
+		if ownerNs != objNs {
+			return fmt.Errorf("cross-namespace owner references are disallowed, owner's namespace %s, obj's namespace %s", owner.GetNamespace(), controlled.GetNamespace())
+		}
+	}
+
+	gvk, err := apiutil.GVKForObject(ro, scheme)
+	if err != nil {
+		return err
+	}
+
+	// Create a new ref
+	ref := *metav1.NewControllerRef(owner, schema.GroupVersionKind{Group: gvk.Group, Version: gvk.Version, Kind: gvk.Kind})
+
+	existingRefs := controlled.GetOwnerReferences()
+	fi := -1
+	for i, r := range existingRefs {
+		if referSameObject(ref, r) {
+			fi = i
+		}
+	}
+	if fi == -1 {
+		existingRefs = append(existingRefs, ref)
+	} else {
+		existingRefs[fi] = ref
+	}
+
+	// Update owner references
+	controlled.SetOwnerReferences(existingRefs)
+	return nil
+}
+
+func referSameObject(a, b metav1.OwnerReference) bool {
+	aGV, err := schema.ParseGroupVersion(a.APIVersion)
+	if err != nil {
+		return false
+	}
+
+	bGV, err := schema.ParseGroupVersion(b.APIVersion)
+	if err != nil {
+		return false
+	}
+
+	return aGV.Group == bGV.Group && a.Kind == b.Kind && a.Name == b.Name
+}
